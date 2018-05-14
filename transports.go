6a4a4a69ad1d0dbe3dd74c6eec35d6d96ac47b3e
Bradley Weston <hello@bweston.me>
2018-05-14 13:55:31 +0100

    Add HTTP server.

--- transports.go
+++ transports.go
@@ -1,17 +1,17 @@
 package servers
 
 import (
+	"context"
 	"net"
+	"net/http"
 
 	"google.golang.org/grpc"
 )
 
-type (
-	grpcTransportMananger struct {
-		addr   string
-		server *grpc.Server
-	}
-)
+type grpcTransportMananger struct {
+	addr   string
+	server *grpc.Server
+}
 
 func (t *grpcTransportMananger) Start() <-chan error {
 	errC := make(chan error)
@@ -42,6 +42,46 @@ func WithGRPCServer(addr string, server *grpc.Server) Option {
 
 	return func(o *serverOptions) error {
 		o.transports = append(o.transports, &grpcTransportMananger{
+			addr:   addr,
+			server: server,
+		})
+		return nil
+	}
+}
+
+type httpTransportMananger struct {
+	addr   string
+	server *http.Server
+}
+
+func (t *httpTransportMananger) Start() <-chan error {
+	errC := make(chan error)
+
+	go func() {
+		nl, err := net.Listen("tcp", t.addr)
+		if err != nil {
+			errC <- err
+			return
+		}
+
+		errC <- t.server.Serve(nl)
+	}()
+
+	return (<-chan error)(errC)
+}
+
+func (t *httpTransportMananger) Stop() error {
+	return t.server.Shutdown(context.Background())
+}
+
+// WithHTTPServer will run a http server when the Server is Run
+func WithHTTPServer(addr string, server *http.Server) Option {
+	if server == nil {
+		panic("must provide both a net listener and HTTP server to WithHTTPServer")
+	}
+
+	return func(o *serverOptions) error {
+		o.transports = append(o.transports, &httpTransportMananger{
 			addr:   addr,
 			server: server,
 		})
